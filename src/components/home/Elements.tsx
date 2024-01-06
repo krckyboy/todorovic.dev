b4137aa1213aaac81defd86a940d1a5d722ac53b
krcky <todorovic_dusan@icloud.com>
2024-01-06 15:28:19 +0100

    #5 Removing some elements.

--- src/components/home/Elements.tsx
+++ src/components/home/Elements.tsx
@@ -3,17 +3,8 @@ import styles from './styles.module.scss';
 
 const Elements = () => {
   const elements = [
-    { type: 'PurpleRectangle',
-      style: {
-        width: '72.277px',
-        height: '122.695px',
-        transform: 'rotate(-50.599deg)',
-        background: '#9B51E0',
-        left: '2%',
-        top: '44%'
-      }
-    },
-    { type: 'GreenEllipse',
+    {
+      type: 'GreenEllipse',
       style: {
         width: '68.697px',
         height: '68.697px',
@@ -23,59 +14,30 @@ const Elements = () => {
         top: '84%'
       }
     },
-    { type: 'YellowRectangle',
+    {
+      type: 'YellowRectangle',
       style: {
         width: '72.277px',
         height: '28.492px',
         transform: 'rotate(45.268deg)',
         backgroundColor: '#F2C94C',
-        left: '44%',
-        top: '25%'
+        left: '37%',
+        top: '13%'
       }
     },
-    { type: 'BlueRectangle',
-      style: {
-        width: '62.109px',
-        height: '59.671px',
-        transform: 'rotate(45.268deg)',
-        backgroundColor: '#2D9CDB',
-        left: '49%',
-        top: '80%'
-      }
-    },
-    { type: 'RedRectangle',
+    {
+      type: 'RedRectangle',
       style: {
         width: '46.5px',
         height: '198.068px',
         transform: 'rotate(21.923deg)',
         backgroundColor: '#EB5757',
         left: '78%',
-        top: '18%'
+        top: '13%'
       }
     },
-    { type: 'BlueTriangle',
-      style: {
-        width: '0',
-        height: '0',
-        borderLeft: '50px solid transparent',
-        borderRight: '50px solid transparent',
-        borderBottom: '100px solid #2F80ED',
-        transform: 'rotate(164.252deg)',
-        right: '18%',
-        top: '61%'
-      }
-    },
-    { type: 'OrangeEllipse',
-      style: {
-        width: '84px',
-        height: '84px',
-        backgroundColor: '#F2994A',
-        borderRadius: '100%',
-        right: '2%',
-        top: '44%'
-      }
-    },
-    { type: 'MintRectangle',
+    {
+      type: 'MintRectangle',
       style: {
         width: '75.945px',
         height: '72.964px',
