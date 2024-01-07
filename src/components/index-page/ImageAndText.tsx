546f55746d41934cda792d7a5fd855084aed78fa
krcky <todorovic_dusan@icloud.com>
2024-01-07 18:38:41 +0100

    Adding some content to landing page and changing main container width.

--- src/components/index-page/ImageAndText.tsx
+++ src/components/index-page/ImageAndText.tsx
@@ -48,15 +48,31 @@ const ImageAndText: FunctionComponent = () => {
           </a>
         </motion.p>
         <motion.section className={styles.paragraphBelow}
-                  initial="hidden"
-                  animate="visible"
-                  variants={variants}
-                  transition={{ duration: 0.5, delay: 1 }}>
-          <p>Bacon ipsum dolor amet nulla corned beef occaecat do aliqua sausage. Bresaola velit commodo pork belly. Ad eiusmod sunt tenderloin officia cupidatat excepteur pork chop.</p>
+                        initial="hidden"
+                        animate="visible"
+                        variants={variants}
+                        transition={{ duration: 0.5, delay: 1 }}>
+          <p>Hello, internet! I'm currently engaged in a rewarding role as a dedicated Front-End Team Lead.
+            I thrive on diving deep into the code and have a proven track record in web development. While I'm deeply
+            committed to my current project, I'm always open to learning about new opportunities that align with my
+            skills and interests.</p>
 
-          <p>Ex nostrud aute, meatball venison doner turkey sed fatback commodo qui ground round.</p>
+          <p>I've spent the last few years honing my <Link href={'/skills'}>skills</Link> and working on some pretty
+            cool projects. Additionally, I've had the privilege of leading a team of exceptional developers, overseeing
+            project management, and ensuring seamless operations.</p>
 
-          <p>Rump officia salami in deserunt non bresaola, short ribs ullamco pork belly t-bone ipsum fatback velit. Qui frankfurter swine sirloin cupim pariatur salami irure pig officia proident.</p>
+          <p>But I'm not just about making things work. In addition to my professional experience, I also share insights
+            on various aspects of web development on my <Link href={'/blog'}>blog</Link>. Whether you're interested in
+            learning more about my technical expertise or improving your own soft skills, there's something for
+            everyone.</p>
+
+          <p>Whether you're a company seeking a dedicated, experienced developer who loves what they do or an individual
+            client valuing quality, creativity, and dedication in their web solutions, I'd be thrilled to connect with
+            you. <a href="https://www.linkedin.com/in/dusan-todorovic-krcky/">Let's chat</a> and see how we can make
+            your project even better!</p>
+
+          <p>If you would like to learn more about me, feel free to roam around or just head to the <Link
+            href={'/about'}>about page</Link>.</p>
         </motion.section>
       </section>
     </div>
