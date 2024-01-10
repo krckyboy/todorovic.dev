f4ba876330f8095ba5b36cda36be76d7007ccfeb
krcky <todorovic_dusan@icloud.com>
2024-01-10 06:08:05 +0100

    Added a section Featured Posts.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,21 @@ import { NextPage } from 'next';
 import ImageAndText from '@/components/image-and-text/ImageAndText';
 import styles from './styles.module.scss';
 import gStyles from '@/styles/global.module.scss';
+import BlogPostItem from '@/components/blog-post-item/BlogPostItem';
+import Link from 'next/link';
 
 const Page: NextPage = () => {
   return (
     <main>
       <section className={`${styles.section} ${gStyles.paddingInline}`}>
         <ImageAndText />
+      </section>
+      <section className={`${styles.section} ${gStyles.paddingInline}`}>
+        <h2 className={styles.featuredPostsHeading}>Featured posts</h2>
+        <div className={styles.featuredPosts}>
+          {[0, 1, 2].map((item) => <BlogPostItem key={item} />)}
+          <Link href={'/blog'} className={styles.checkAllPosts}>Check all</Link>
+        </div>
       </section>
     </main>
   );
