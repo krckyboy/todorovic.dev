ecea799728cf9c966e82aa0fd6a64770088d7bb7
krckyboy <dusan.todorovic.2016@gmail.com>
2024-01-14 16:24:13 +0100

    #7 Adding try catch blocks.

--- next/src/scripts/fetch.ts
+++ next/src/scripts/fetch.ts
@@ -1,19 +1,24 @@
 import type { PostsFetchResponse } from '@/components/blog-post-item/types';
 
 export const fetchWrapper = async <T>(url: string | URL) => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_URL}${url}`, {
-    credentials: 'include',
-    headers: {
-      'Authorization': `Bearer ${process.env.NEXT_PUBLIC_STRAPI_TOKEN}`
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_URL}${url}`, {
+      credentials: 'include',
+      headers: {
+        'Authorization': `Bearer ${process.env.NEXT_PUBLIC_STRAPI_TOKEN}`
+      }
+    });
+
+    if (!response.ok) {
+      const txt = await response.text();
+      throw new Error(txt);
     }
-  });
 
-  if (!response.ok) {
-    const txt = await response.text();
-    throw new Error(txt);
+    return await response.json() as T;
+  } catch (error) {
+    console.error('Fetch error:', error);
+    throw error;
   }
-
-  return await response.json() as T;
 };
 
 export const db = {
