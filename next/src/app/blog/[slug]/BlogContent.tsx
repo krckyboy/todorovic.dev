5847230733a718a59910176c25136f0735c47c0a
krckyboy <dusan.todorovic.2016@gmail.com>
2024-01-14 19:05:15 +0100

    #7 Added syntax highlight.

--- next/src/app/blog/[slug]/BlogContent.tsx
+++ next/src/app/blog/[slug]/BlogContent.tsx
@@ -1,5 +1,7 @@
 import React, { FunctionComponent } from 'react';
 import ReactMarkdown from 'react-markdown';
+import rehypeHighlight from 'rehype-highlight';
+import 'highlight.js/styles/github-dark-dimmed.css';
 
 interface Props {
   markdown: string;
@@ -7,7 +9,9 @@ interface Props {
 
 const BlogContent: FunctionComponent<Props> = async ({ markdown }) => {
   return (
-    <ReactMarkdown>{markdown}</ReactMarkdown>
+    <div style={{ maxWidth: '100%', overflowX: 'auto' }}>
+      <ReactMarkdown rehypePlugins={[rehypeHighlight]}>{markdown}</ReactMarkdown>
+    </div>
   );
 };
 
