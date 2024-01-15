3c89933d0f3c51855452a6bf5f2582790b34248a
krckyboy <dusan.todorovic.2016@gmail.com>
2024-01-15 21:54:51 +0100

    #10 Refactoring folder structure.

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -1,30 +1,13 @@
 import React from 'react';
 import { NextPage } from 'next';
-import ImageAndText from '../components/image-and-text/ImageAndText';
-import styles from './styles.module.scss';
-import gStyles from '../styles/global.module.scss';
-import BlogPostItem from '@/components/blog-post-item/BlogPostItem';
-import Link from 'next/link';
-import { db } from '@/scripts/fetch';
+import ImageAndText from '@/components/home-components/image-and-text/ImageAndText';
+import FeaturedPosts from '@/components/home-components/featured-posts/FeaturedPosts';
 
 const Page: NextPage = async () => {
-  const posts = await db.getFeaturedPosts();
-
   return (
     <main>
-      <section className={`${gStyles.section} ${gStyles.paddingInline}`}>
-        <ImageAndText />
-      </section>
-      <section className={`${gStyles.section} ${gStyles.paddingInline}`}>
-        <h2 className={`${styles.featuredPostsHeading} ${gStyles.pageHeadingMini}`}>Featured posts</h2>
-        <p className={'text'}>Ever feel like writing about something intriguing or useful? That's me! I'm all about
-          exploring topics that spark my curiosity or could help fellow developers. Always open to sharing my insights
-          and experiences.</p>
-        <div className={`${gStyles.blogs} ${styles.blogs}`}>
-          {posts.data.map((post) => <BlogPostItem post={post} key={post.id} featured />)}
-          <Link href={'/blog'} className={styles.checkAllPosts}>Check all</Link>
-        </div>
-      </section>
+      <ImageAndText />
+      <FeaturedPosts />
     </main>
   );
 };
---
+++ next/src/components/home-components/image-and-text/ImageAndText.tsx
@@ -0,0 +1,50 @@
+'use client';
+import React, { FunctionComponent } from 'react';
+import styles from './styles.module.scss';
+import Link from 'next/link';
+import gStyles from '@/styles/global.module.scss';
+
+const ImageAndText: FunctionComponent = () => {
+  return (
+    <section className={`${gStyles.section} ${gStyles.paddingInline}`}>
+      <div className={styles.container}>
+        <img src={'images/dusan.png'}
+             alt={'Dušan Todorović in a formal white shirt'}
+             width={200}
+             height={200}
+             className={styles.image}
+        />
+        <section className={styles.textSection}>
+          <h1 className={styles.title}
+          >
+            <Link href={'/about'} className={styles.accented}>Dušan Todorović</Link>
+          </h1>
+          <p className={styles.tags}>
+            Developer <span>|</span> Team Leader <span>|</span> React Specialist
+          </p>
+          <section className={`${styles.paragraphBelow} text`}>
+            <p>Hello, internet! I'm Dušan Todorović, a dedicated Web Developer based in <a
+              className={styles.locationLink}
+              href={'https://maps.app.goo.gl/DHTdsDU24RjWYhhV8'}
+              target={'_blank'}>
+              Belgrade, Serbia
+            </a>. I specialize in diving deep into the code
+              and leading teams to ensure smooth project execution.</p>
+
+            <ul className={styles.ul}>
+              <li>Interested in my skills? Visit my <Link href={'/skills'}>skills page</Link>, or if you prefer - <Link
+                href={'/cv'}>download my CV</Link>.
+              </li>
+              <li>If you're a company or individual client looking for a dedicated, experienced developer, <Link
+                href="https://www.linkedin.com/in/dusan-todorovic-krcky/">let's chat</Link>! Let's make your project
+                better... or at least, less buggy!
+              </li>
+            </ul>
+          </section>
+        </section>
+      </div>
+    </section>
+  );
+};
+
+export default ImageAndText;
