6f5c6d86e8c8cddd84b73694fb933553e7be3bb7
krckyboy <dusan.todorovic.2016@gmail.com>
2024-02-01 20:55:23 +0100

    #10 Deleting unused photos.

--- next/src/app/about/image-section/data.ts
+++ next/src/app/about/image-section/data.ts
@@ -5,28 +5,16 @@ interface Item {
 
 const data: Item[] = [
   {
-    src: '/images/about/846FD215-D652-4225-9AC1-BDB43CCC56DF_1_105_c.jpeg',
-    alt: 'Dušan in a gondola.'
-  },
-  {
-    src: '/images/about/E7FF333A-32F1-4E1A-B624-8FD917A8F194_1_105_c.jpeg',
-    alt: '/images/about/3D037048-F26D-464D-BB7E-13AA8696C6B9_1_105_c.jpeg'
-  },
-  {
     src: '/images/about/3D037048-F26D-464D-BB7E-13AA8696C6B9_1_105_c.jpeg',
     alt: 'Dušan on the beach.'
-  },
-  {
-    src: '/images/about/2F6ACD0F-D23F-4006-97D8-5D062B0EB330_1_105_c.jpeg',
-    alt: 'Dušan next to an elephant.'
   },
   {
     src: '/images/about/EF46B464-0160-404D-B2B4-33224F118617_1_105_c.jpeg',
     alt: 'Lilly on a sofa.'
   },
   {
-    src: '/images/about/27318BF1-3FDE-4F44-99AB-301A371463F0_1_105_c.jpeg',
-    alt: 'Lilly on my lap.'
+    src: '/images/about/2F6ACD0F-D23F-4006-97D8-5D062B0EB330_1_105_c.jpeg',
+    alt: 'Dušan next to an elephant.'
   }
 ];
 
