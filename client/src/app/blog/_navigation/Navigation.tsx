281b26d3c26b6a7b79ae7f9f1444fca438f7f76f
krckyboy <dusan.todorovic.2016@gmail.com>
2024-02-26 10:22:32 +0100

    #27 Added actual numbers with ...

--- client/src/app/blog/_navigation/Navigation.tsx
+++ client/src/app/blog/_navigation/Navigation.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useMemo } from 'react';
 import styles from './styles.module.scss';
 import Link from 'next/link';
 import qs from 'qs';
@@ -12,8 +12,8 @@ interface Props {
 const Navigation: FunctionComponent<Props> = ({ currentPageCount, pageNumber, category }) => {
   const rangeStart = 1;
   const rangeEnd = currentPageCount;
+  const maxPagesToShow = 3;
 
-  // Function to generate query string for a specific page
   const generateQueryString = (page: number) => {
     const queryParams = {
       category,
@@ -22,6 +22,50 @@ const Navigation: FunctionComponent<Props> = ({ currentPageCount, pageNumber, ca
     return qs.stringify(queryParams);
   };
 
+  const generatePageLinks = () => {
+    const links = [];
+    let start = Math.max(rangeStart, pageNumber - Math.floor(maxPagesToShow / 2));
+    let end = Math.min(rangeEnd, start + maxPagesToShow - 1);
+
+    if (end - start < maxPagesToShow - 1) {
+      start = Math.max(rangeStart, end - maxPagesToShow + 1);
+    }
+
+    if (start > rangeStart) {
+      links.push(
+        <Link key="start" href={`/blog?${generateQueryString(rangeStart)}`}>
+          {rangeStart}
+        </Link>
+      );
+      if (start > rangeStart + 1) {
+        links.push(<span key="ellipsis-start">...</span>);
+      }
+    }
+
+    for (let i = start; i <= end; i++) {
+      links.push(
+        <Link key={i} href={`/blog?${generateQueryString(i)}`} className={i === pageNumber ? styles.active : ''}>
+          {i}
+        </Link>
+      );
+    }
+
+    if (end < rangeEnd) {
+      if (end < rangeEnd - 1) {
+        links.push(<span key="ellipsis-end">...</span>);
+      }
+      links.push(
+        <Link key="end" href={`/blog?${generateQueryString(rangeEnd)}`}>
+          {rangeEnd}
+        </Link>
+      );
+    }
+
+    return links;
+  };
+
+  const memoizedGeneratePageLinks = useMemo(() => generatePageLinks(), [rangeStart, pageNumber, rangeEnd, maxPagesToShow]);
+
   return (
     <div className={styles.container}>
       {pageNumber > 1 && (
@@ -29,13 +73,7 @@ const Navigation: FunctionComponent<Props> = ({ currentPageCount, pageNumber, ca
           Previous
         </Link>
       )}
-      {Array.from({ length: rangeEnd - rangeStart + 1 }, (_, i) => rangeStart + i).map((page) => (
-        <Link key={page}
-              href={`/blog?${generateQueryString(page)}`}
-              className={page === pageNumber ? styles.active : ''}>
-          {page}
-        </Link>
-      ))}
+      {memoizedGeneratePageLinks}
       {currentPageCount > pageNumber && (
         <Link href={`/blog?${generateQueryString(pageNumber + 1)}`}>
           Next
