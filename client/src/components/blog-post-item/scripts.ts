272b43ec8ccdbf55870307c90ee42d7b45b53738
krckyboy <dusan.todorovic.2016@gmail.com>
2024-03-17 23:11:36 +0100

    Deleting client folder.

--- client/src/components/blog-post-item/scripts.ts
+++
@@ -1,9 +0,0 @@
-import { format, formatDistanceToNow } from 'date-fns';
-
-export const convertToHumanReadableDate = (date: string) => {
-  const dateObject = new Date(date);
-  const formattedDate = format(dateObject, 'MMMM dd, yyyy');
-  const timeAgo = formatDistanceToNow(formattedDate, { addSuffix: true });
-
-  return `${formattedDate} (${timeAgo})`;
-};
