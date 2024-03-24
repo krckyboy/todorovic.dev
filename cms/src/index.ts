202baa5f8c16d291c61fadecdcbb2e8cc0d0024c
krckyboy <dusan.todorovic.2016@gmail.com>
2024-03-24 15:36:38 +0100

    #44 Added seeding logic in bootstrap section.

--- cms/src/index.ts
+++ cms/src/index.ts
@@ -1,3 +1,37 @@
+import { faker } from '@faker-js/faker';
+
+async function seedCategories() {
+  for (let i = 0; i < 5; i++) {
+    await strapi.db.query('api::category.category').create({
+      data: {
+        name: faker.commerce.department().toLowerCase()
+      }
+    });
+  }
+
+  console.log('Categories seeded successfully.');
+}
+
+async function seedPosts() {
+  const categories = await strapi.db.query('api::category.category').findMany();
+
+  for (let i = 0; i < 20; i++) {
+    const category = faker.helpers.arrayElement(categories);
+
+    await strapi.db.query('api::post.post').create({
+      data: {
+        title: faker.lorem.sentence({ max: 10, min: 3 }),
+        isFeatured: faker.datatype.boolean(),
+        content: faker.lorem.paragraphs(15).split('\n').join('\n\n'),
+        categories: [category.id],
+        viewCount: faker.number.int({ min: 0, max: 1000 })
+      }
+    });
+  }
+
+  console.log('Posts seeded successfully.');
+}
+
 export default {
   /**
    * An asynchronous register function that runs before
@@ -5,7 +39,8 @@ export default {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/*{ strapi }*/) {},
+  register(/*{ strapi }*/) {
+  },
 
   /**
    * An asynchronous bootstrap function that runs before
@@ -14,5 +49,17 @@ export default {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  async bootstrap(/*{ strapi }*/) {
+    // Check if the database has already been seeded
+    const postsCount = await strapi.db.query('api::post.post').count();
+    const categoriesCount = await strapi.db.query('api::category.category').count();
+
+    if (categoriesCount < 5) {
+      await seedCategories();
+    }
+
+    if (postsCount < 20) {
+      await seedPosts();
+    }
+  }
 };
