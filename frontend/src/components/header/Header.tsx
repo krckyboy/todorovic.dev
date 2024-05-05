de2df3be7bce2751e42a2059edaa06be79202485
krckyboy <dusan.todorovic.2016@gmail.com>
2024-05-05 21:15:16 +0200

    #63 Adding conditional rendering of blog link + small fix in search bar.

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -2,8 +2,12 @@ import React, { FunctionComponent } from 'react';
 import styles from './styles.module.scss';
 import gStyles from '@/styles/global.module.scss';
 import NavLink from '@/components/header/nav-link/NavLink';
+import { db } from '@/scripts/fetch';
 
-const Header: FunctionComponent = () => {
+const Header: FunctionComponent = async () => {
+  const posts = await db.getPosts(0);
+  const hasAnyPosts = posts?.data.length;
+
   return (
     <header className={`${styles.container} ${gStyles.paddingInline} ${gStyles.text}`}>
       <nav className={styles.nav}>
@@ -11,7 +15,9 @@ const Header: FunctionComponent = () => {
           <NavLink href={'/'}>Home</NavLink>
           <NavLink href={'/skills'}>Skills</NavLink>
           <NavLink href={'/about'}>About</NavLink>
-          <NavLink href={'/blog'}>Blog</NavLink>
+          {hasAnyPosts && (
+            <NavLink href={'/blog'}>Blog</NavLink>
+          )}
         </ul>
       </nav>
     </header>
