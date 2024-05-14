11aabeb7981e5fcb22c7cc20a335f1e66581ee47
krckyboy <dusan.todorovic.2016@gmail.com>
2024-05-14 21:06:34 +0200

    #68 SEO changes.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -10,8 +10,20 @@ import LoadingIndicator from '@/components/loading-indicator/LoadingIndicator';
 const leagueSpartan = League_Spartan({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Dušan Todorović - Developer | Team Leader | React Specialist',
-  description: 'Created by Dušan Todorović'
+  title: 'Dušan Todorović | Senior Software Engineer & Team Leader',
+  description: 'Discover Dušan Todorović, a seasoned Software Engineer and Team Leader specializing in React. Explore his expertise, leadership, and contributions to the tech industry.',
+  twitter: {
+    title: 'Dušan Todorović | Senior Software Engineer & Team Leader',
+    description: 'Join Dušan Todorović on his journey as a Software Engineer and Team Leader, specializing in React. Learn about his insights and impact on the tech world.',
+    images: 'images/dusan.png'
+  },
+  openGraph: {
+    title: 'Dušan Todorović | Senior Software Engineer & Team Leader',
+    description: 'Dive into the professional journey of Dušan Todorović, a Software Engineer and Team Leader with a focus on React. Discover his expertise and leadership in the tech industry.',
+    images: 'images/dusan.png',
+    type: 'website',
+    url: 'https://todorovic.dev'
+  }
 };
 
 interface Props {
