ff4570779f13d22eb9a0ed137e8ab6691ed631c7
krckyboy <dusan.todorovic.2016@gmail.com>
2024-05-14 21:36:53 +0200

    SEO changes: adding og.png

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -15,13 +15,13 @@ export const metadata: Metadata = {
   twitter: {
     title: 'Dušan Todorović | Senior Software Engineer & Team Leader',
     description: 'Join Dušan Todorović on his journey as a Software Engineer and Team Leader, specializing in React. Learn about his insights and impact on the tech world.',
-    images: 'https://www.todorovic.dev/images/dusan.png',
+    images: ['https://www.todorovic.dev/images/dusan.png', 'https://www.todorovic.dev/images/og.png'],
     card: 'summary_large_image'
   },
   openGraph: {
     title: 'Dušan Todorović | Senior Software Engineer & Team Leader',
     description: 'Dive into the professional journey of Dušan Todorović, a Software Engineer and Team Leader with a focus on React. Discover his expertise and leadership in the tech industry.',
-    images: 'https://www.todorovic.dev/images/dusan.png',
+    images: ['https://www.todorovic.dev/images/dusan.png', 'https://www.todorovic.dev/images/og.png'],
     type: 'website',
     url: 'https://todorovic.dev'
   }
