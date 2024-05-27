d3273eaea5e30858dbb25f5c5d8762d1b0ba026a
krckyboy <dusan.todorovic.2016@gmail.com>
2024-05-27 21:05:20 +0200

    #79 Setting up image domains.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,22 @@
 const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'krcky-dev-production.up.railway.app',
+        port: '',
+        pathname: '/uploads/**'
+      },
+      {
+        protocol: 'http',
+        hostname: 'cms',
+        port: '1337',
+        pathname: '/uploads/**'
+      }
+    ]
+  },
   webpack: (config) => {
     config.resolve.alias['@'] = path.join(__dirname, 'src');
     return config;
