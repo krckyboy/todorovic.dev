32009d2330c09b0afd7709e7e1aaafdb8b63fad2
krckyboy <dusan.todorovic.2016@gmail.com>
2024-06-02 08:55:27 +0200

    Tweaking tags.

--- frontend/src/components/author-card/AuthorCard.tsx
+++ frontend/src/components/author-card/AuthorCard.tsx
@@ -19,7 +19,7 @@ const AuthorCard: FunctionComponent = () => {
           <span className={styles.name}>Dušan Todorović</span>
         </Link>
         <p className={`${gStyles.tagsMini}`}>
-          Developer <span>|</span> Team Leader <span>|</span> React Specialist
+          Software Engineer <span>|</span> Team Leader
         </p>
         <ul className={`${gStyles.tagsMini} ${styles.links}`}>
           <li>
