d655111d52bee2aaa3faf06f9f5a443071d9f35d
krckyboy <dusan.todorovic.2016@gmail.com>
2024-06-04 21:44:30 +0200

    #93 Fixing layout.

--- frontend/src/app/skills/page.tsx
+++ frontend/src/app/skills/page.tsx
@@ -4,11 +4,10 @@ import styles from './styles.module.scss';
 import React from 'react';
 import Experience from '@/app/skills/_experience/Experience';
 import Skills from '@/app/skills/_skills/Skills';
-import sharedStyles from '@/styles/about-and-blog-content.module.scss';
 
 const Page: NextPage = () => {
   return (
-    <main className={`${styles.main} ${gStyles.section} ${gStyles.text} ${gStyles.mainContainer} ${sharedStyles.shared}`}>
+    <main className={`${styles.main} ${gStyles.section} ${gStyles.text} ${gStyles.mainContainer} `}>
       <h1 className={gStyles.pageHeading}>Skills</h1>
       <Skills />
       <Experience />
