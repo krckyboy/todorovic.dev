677fb2565b08b6375799c7ea8740d3d81a964300
krckyboy <dusan.todorovic.2016@gmail.com>
2024-06-04 21:10:11 +0200

    #93 Tweaking fonts for 4k. Kinda hotfix and messy, but it will work for now.

--- frontend/src/app/skills/page.tsx
+++ frontend/src/app/skills/page.tsx
@@ -4,10 +4,11 @@ import styles from './styles.module.scss';
 import React from 'react';
 import Experience from '@/app/skills/_experience/Experience';
 import Skills from '@/app/skills/_skills/Skills';
+import sharedStyles from '@/styles/about-and-blog-content.module.scss';
 
 const Page: NextPage = () => {
   return (
-    <main className={`${styles.main} ${gStyles.section} ${gStyles.text} ${gStyles.mainContainer}`}>
+    <main className={`${styles.main} ${gStyles.section} ${gStyles.text} ${gStyles.mainContainer} ${sharedStyles.shared}`}>
       <h1 className={gStyles.pageHeading}>Skills</h1>
       <Skills />
       <Experience />
